325cec7ae1697bed1adecd90b7655d76965b1566
harr1626 <harr1626@PCB-LAB360-23>
2019-05-01 10:51:39 -0500

    kvh

---
+++ StudyGuide/StudyGuide/IMDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyGuide
+{
+    class IMDB
+    {
+      public int movie_Id                   {get; set;}
+      public int director_name              {get;set;}
+      public int num_critic_for_reviews     {get;set;}
+      public int duration                   {get;set;}
+      public int gross                      {get;set;}
+      public int genres                     {get;set;}
+      public int actor_1_name               {get;set;}
+      public int movie_title                {get;set;}
+      public int num_voted_users            {get;set;}
+      public int cast_total_facebook_likes  {get;set;}
+      public int plot_keywords              {get;set;}
+      public int movie_imdb_link            {get;set;}
+      public int num_user_for_reviews       {get;set;}
+      public int language                   {get;set;}
+      public int country                    {get;set;}
+      public int content_rating             {get;set;}
+      public int budget                     {get;set;}
+      public int title_year                 {get;set;}
+      public int imdb_score                 {get;set;}
+      public int movie_facebook_likes { get; set; }
+    }
+}
